46902aaf4bc9ecaf25645311cbc42f8bb7fe63e5
Minh Khai <khaile.03042005@gmail.com>
2025-09-03 21:48:32 +0700

    create project

---
+++ src/main/java/com/grabtutor/grabtutor/GrabtutorApplication.java
@@ -0,0 +1,13 @@
+package com.grabtutor.grabtutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabtutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabtutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grabtutor/grabtutor/GrabtutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabtutor.grabtutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabtutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
