df6677f27c76a0c3cbb12a20b0c96289fbd5f611
Minh Khai <khaile.03042005@gmail.com>
2025-09-10 11:08:54 +0700

    update service

---
+++ src/main/java/com/grabtutor/grabtutor/service/impl/UserServiceImpl.java
@@ -0,0 +1,70 @@
+package com.grabtutor.grabtutor.service.impl;
+
+import com.grabtutor.grabtutor.dto.request.UserRequest;
+import com.grabtutor.grabtutor.dto.response.UserResponse;
+import com.grabtutor.grabtutor.entity.User;
+import com.grabtutor.grabtutor.exception.AppException;
+import com.grabtutor.grabtutor.exception.ErrorCode;
+import com.grabtutor.grabtutor.mapper.UserMapper;
+import com.grabtutor.grabtutor.repository.UserRepository;
+import com.grabtutor.grabtutor.service.UserService;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserServiceImpl implements UserService {
+    UserRepository userRepository;
+    UserMapper userMapper;
+
+    public UserResponse addUser(UserRequest userRequest){
+        if(userRepository.existsByUsername(userRequest.getUsername())){
+            throw new AppException(ErrorCode.USERNAME_ALREADY_EXISTS);
+        }
+        if(userRepository.existsByEmail(userRequest.getEmail())){
+            throw new AppException(ErrorCode.EMAIL_ALREADY_EXISTS);
+        }
+
+        User user = userMapper.toUser(userRequest);
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+
+    public List<UserResponse> getAllUsers(){
+        List<User> users = userRepository.findAll();
+        return users.stream().map(userMapper::toUserResponse).toList();
+    }
+
+    public UserResponse getUserById(String id){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        return userMapper.toUserResponse(user);
+    }
+
+    public UserResponse updateUser(String id, UserRequest userRequest) {
+        User user = userRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        if (!user.getUsername().equals(userRequest.getUsername()) && userRepository.existsByUsername(userRequest.getUsername())) {
+            throw new AppException(ErrorCode.USERNAME_ALREADY_EXISTS);
+        }
+        if (!user.getEmail().equals(userRequest.getEmail()) && userRepository.existsByEmail(userRequest.getEmail())) {
+            throw new AppException(ErrorCode.EMAIL_ALREADY_EXISTS);
+        }
+        userMapper.updateUserFromRequest(userRequest, user);
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    public void deleteUser(String id){
+        User user = userRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        userRepository.delete(user);
+    }
+
+    public UserResponse changeActive(String id, boolean active){
+        User user = userRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        user.setActive(active);
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+}
