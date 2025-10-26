f3c20bdc452889b1530cfd5b5e80b6c78c5a212e
KhoaOoshio <khoatrancraft2@gmail.com>
2025-10-26 18:26:23 +0700

    add to fetch

--- src/main/java/com/grabtutor/grabtutor/service/UserService.java
+++ src/main/java/com/grabtutor/grabtutor/service/UserService.java
@@ -20,11 +20,10 @@ public interface UserService {
     PageResponse<?> getAllUsers(int pageNo, int pageSize, String... sortBy);
 
     UserResponse getMyInfo();
-    public TutorResponse addTutor(TutorRequest request);
+
+    TutorResponse addTutor(TutorRequest request);
 
     TutorInfoResponse updateTutorInfo(TutorInfoRequest tutorInfoRequest);
-
-//    AccountVerificationResponse submitRequest(AccountVerificationRequest request);
 
     ApproveResponse approveRequest(ApproveRequest request);
 
