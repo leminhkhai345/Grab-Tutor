dcca2aaa17298947ae5632c82ea126deab249f4f
KhoaOoshio <khoatrancraft2@gmail.com>
2025-10-29 19:43:52 +0700

    Xử lý lỗi remove session nêu connect không được

--- src/main/java/com/grabtutor/grabtutor/dto/response/MessageResponse.java
+++ src/main/java/com/grabtutor/grabtutor/dto/response/MessageResponse.java
@@ -1,7 +1,9 @@
 package com.grabtutor.grabtutor.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
@@ -10,6 +12,8 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageResponse {
     String userId;
     String roomId;
