e61d9089471233c99c4df1a230d6cce84942b29b
KhoaOoshio <khoatrancraft2@gmail.com>
2025-11-12 21:25:58 +0700

    fix lỗi trả tin nhắn + room theo thời gian

--- src/main/java/com/grabtutor/grabtutor/repository/MessageRepository.java
+++ src/main/java/com/grabtutor/grabtutor/repository/MessageRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, String> {
-    List<Message> findByChatRoomId(String roomId);
+    List<Message> findByChatRoomIdOrderByCreatedAtAsc(String roomId);
 }
